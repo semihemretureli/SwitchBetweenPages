b4833059b1d394c4f1485f29548f96351276c4e3
Semih <semihemretureli@gmail.com>
2022-10-26 23:45:22 +0300

    firebase connection made

--- app/src/main/java/com/semih/switchbetweenpages/MainActivity.java
+++ app/src/main/java/com/semih/switchbetweenpages/MainActivity.java
@@ -1,19 +1,32 @@
 package com.semih.switchbetweenpages;
 
+import static android.service.controls.ControlsProviderService.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity<EmailPasswordActivity, user> extends AppCompatActivity {
     private EditText userName, password;
     private TextView txtConclusion;
     private String uName, uPassword, trueUName = "Semih", trueUPassword = "12345M";
-
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +35,18 @@ public class MainActivity extends AppCompatActivity {
         userName = (EditText) findViewById(R.id.editTxtUsername);
         password = (EditText) findViewById(R.id.editTxtPassword);
         txtConclusion = (TextView) findViewById(R.id.txtVievConclusion);
+        mAuth = FirebaseAuth.getInstance();
+    }
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            currentUser.reload();
+        }
     }
 
     public void btnLogin(View v) {
@@ -31,18 +55,27 @@ public class MainActivity extends AppCompatActivity {
 
         if (!TextUtils.isEmpty(uName)) {
             if (!TextUtils.isEmpty(uPassword)) {
-                if (uName.equals(trueUName)) {
-                    if (uPassword.equals(trueUPassword)) {
-                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                        intent.putExtra("userName", uName);
-                        finish();
-                        startActivity(intent);
-                    } else {
-                        txtConclusion.setText("Wrong Password.");
+
+                mAuth.signInWithEmailAndPassword(uName, uPassword).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            intent.putExtra("userName", uName);
+                            finish();
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
                     }
-                } else {
-                    txtConclusion.setText("Username is wrong");
-                }
+                });
+
+
             } else {
                 txtConclusion.setText("Password cannot be empty");
             }
@@ -50,4 +83,5 @@ public class MainActivity extends AppCompatActivity {
             txtConclusion.setText("Username cannot be empty");
         }
     }
+
 }
